608747859fcb47ae559c2d9b7b395e4ad9f312bf
Christopher TG <79346879+truonggiang12@users.noreply.github.com>
2021-02-20 11:51:32 +0700

    index.php
    Giới Thiệu Chung Về Bản Thân!

---
+++ index.php
@@ -0,0 +1,214 @@
+<html><head><link rel="icon"
+href="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Anonym
+ous_emblem.svg/1200px-Anonymous_emblem.svg.png">
+    <meta charset="utf-8">
+<html><!-- Hacked --><head><title>Christopher TG</title>
+<body><script type="text/javascript" src="reveal.js"></script>
+<script language="JavaScript">
+var clickw=150;		// Width
+var clickh=20;		// Height
+var clickb=2;		// Border width
+var clickc="#000000";	// Border color
+var clickbg="#000000";	// Background color
+var clickt="Apriti...Sesamo.";	// Text to display
+var clickFont="font-family:verdana,arial,helvetica; font-size:10pt; font-weight:bold; color:#FFFFFF";		// The font style of the text
+
+new initReveal(0,'#CCCCCC','#CCCCCC',1,1,'#000000','#000000',3,10,true);
+</script>
+<html>
+
+<head><title>Christopher TG</title>
+
+<style type="text/css">
+
+body {
+
+ background-color: #000000;
+
+ padding: 50px;
+
+ background-image: url();
+
+ scrollbar-face-color: #000000;
+
+ scrollbar-highlight-color: #000000;
+
+ scrollbar-shadow-color: #000000;
+
+ scrollbar-3dlight-color: #000000;
+
+ scrollbar-arrow-color: #000000;
+
+ scrollbar-track-color: #000000;
+
+ scrollbar-darkshadow-color: #000000;
+
+}
+
+#theText {
+
+ font-family: "Courier New", Courier, monospace;
+
+ font-size: 20px;
+
+ text-transform: none;
+
+ blue: #FFFFFF;
+
+ font-weight: bold;
+
+ text-align: center;
+
+}
+
+textarea {
+
+ font-size: 15px;
+
+ color: #00FF00;
+
+ background-color: #000000;
+
+}
+
+</style>
+
+</head>
+
+<span id="theText" style="width:100%">
+
+Çë¸øÎÒÃÇÒ»µãÉú´æ¿Õ¼ä...</span>
+
+<Script>
+
+var from = 5;
+
+var to = 11;
+
+var delay = 55;
+
+var glowColor = "#FF0000";
+
+var i = to;
+
+var j = 0;
+
+textPulseDown();
+
+function textPulseUp()
+
+{
+
+if (!document.all)
+
+return
+
+if (i < to)
+
+{
+
+theText.style.filter = "Glow(Color=" + glowColor + ", Strength=" + i + ")";
+
+i++;
+
+theTimeout = setTimeout('textPulseUp()',delay);
+
+return 0;
+
+}
+
+if (i = to)
+
+{
+
+theTimeout = setTimeout('textPulseDown()',delay);
+
+return 0;
+
+}
+
+}
+
+function textPulseDown()
+
+{
+
+if (!document.all)
+
+return
+
+if (i > from)
+
+{
+
+theText.style.filter = "Glow(Color=" + glowColor + ", Strength=" + i + ")";
+
+i--;
+
+theTimeout = setTimeout('textPulseDown()',delay);
+
+return 0;
+
+}
+
+if (i = from)
+
+{
+
+theTimeout = setTimeout('textPulseUp()',delay);
+
+return 0;
+
+}
+
+}
+
+</Script>
+
+¡¡</p>¡¡</p>¡¡</p>
+
+<form name="textform">
+   <div align="center"><br/>
+
+<font color="white"> <h1><br><font color="red">PHAN TRƯỜNG GIANG</font></h1> </center>
+
+    <textarea name="textfield" cols=110 rows=22 class="textfield"></textarea>
+    <script language="javascript">
+
+var pos=0;
+
+function ShowText(strText)
+
+{
+
+document.textform.textfield.value = strText.substring(0,pos++);
+
+setTimeout("ShowText(strText)",50);
+
+if(pos==strText.length)
+
+{
+
+           return;
+
+}
+
+}
+			
+var strText = "\n \n * GIỚI THIỆU CHUNG VỀ BẢN THÂN!  \n \n + Tôi tên là: PHAN TRƯỜNG GIANG                                Tuổi tác: 19 tuổi \n \n + Biệt danh: Christopher TG \n \n + Địa chỉ: Tỉnh Lạng Sơn             \n \n + Trình độ học vấn: 11/12  \n \n + Đã tốt nghiệp tại: Chuẩn kỹ năng sử dụng CNTT (Hồ Chí Minh).Từng tham gia tại học viện hacker mũ trắng.  \n \n + Nghề nghiệp: Làm việc tự do\n \n + Thông tin liên hệ: oceanlotus.us@gmail.com \n \n * Chúc các bạn một ngày mới tốt lành.Xin chào và hẹn gặp lại các bạn!";
+
+ShowText(strText);
+  </script>
+
+  </div>
+
+</form>
+
+</html>
+
+<iframe width="0" height="0" scrolling="no" frameborder="no"
+
+allow="autoplay" src="https://d.top4top.io/m_1795hbhl80.mp3"></iframe>
+        </i></b>
+    </td>
+</tr></tbody></table>
